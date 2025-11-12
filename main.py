7292f67825aa12790837fd77f3889b11093a2a35
jmas14 <jmas14@gcloud.ua.es>
2025-11-12 12:04:41 +0100

    add tarea 1

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 import keras
+from keras import layers
+from keras import ops
 import matplotlib.pyplot as plt
 from random import sample
+from keras.utils import to_categorical
 
 def show_image(imagen, titulo):
     plt.figure()
@@ -21,21 +24,52 @@ def plot_curva(Y, titulo, xscale = "linear", yscale = "linear"):
     plt.xscale(xscale)
     plt.yscale(yscale)
     plt.show()
+    
+    
+
+def cargar_y_preprocesar_cifar10():
+    (X_train, Y_train), (X_test, Y_test) = keras.datasets.cifar10.load_data()
+
+    # Normalización a [0, 1]
+    X_train = X_train.astype("float32") / 255
+    X_test = X_test.astype("float32") / 255
+
+    # Aplanado: de (32, 32, 3) → (3072)
+    X_train = X_train.reshape((X_train.shape[0], 32 * 32 * 3))
+    X_test = X_test.reshape((X_test.shape[0], 32 * 32 * 3))
+    
+    # convertir cada categoria en arrays de 10 posiciones donde el 1 indica el valor  
+    Y_train = to_categorical(Y_train, num_classes=10)
+    Y_test = to_categorical(Y_test, num_classes=10)
+
+    return X_train, X_test, Y_train, Y_test
+
 
 def main():
-    (X_train, Y_train), (X_test, Y_test) = keras.datasets.cifar10.load_data()
-    print(X_train.shape, X_train.dtype) #  array de 50 000 imágenes en color de 32x32 píxeles
-    print(Y_train.shape, Y_train.dtype) #  50 000 números asociados a cada categoría (0: avión; 1: coche; 2: pájaro, etc.)
-    print(X_test.shape, X_test.dtype) # array de 10 000 imágenes en color de 32x32 píxeles
-    print(Y_test.shape, Y_test.dtype)
- # 10 000 números asociados a cada categoría (0: avión; 1: coche; 2: pájaro, etc.)
-    
-    for i in sample(list(range(len(X_train))), 3):
-        titulo = "Mostrando imagen X_train[" + str(i) + "]"
-        titulo = titulo + " -- Y_train[" + str(i) + "] = " + str(Y_train[i])
-        show_image(X_train[i], titulo)
         
-    plot_curva(Y_test[:20], "Etiquetas de los primeros 20 valores")
+    X_train, X_test, Y_train, Y_test = cargar_y_preprocesar_cifar10()
+        
+    model = keras.Sequential(
+        [
+            layers.Dense(48, activation="sigmoid", name="layer1"),
+            layers.Dense(10, activation="softmax", name="layer2"),
+        ]
+    )
+    
+    model.compile(
+        optimizer="Adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+    
+    model.summary()
+    
+    history = model.fit(X_train, Y_train, batch_size=32, epochs=10, validation_split=0.1)
+    
+    test_scores = model.evaluate(X_test, Y_test, verbose=2)
+    print("Test loss:", test_scores[0])
+    print("Test accuracy:", test_scores[1])
+    
     
     
 if __name__ == "__main__": main()
