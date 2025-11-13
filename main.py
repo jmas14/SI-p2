8ab6bddc05fb60c4b8d122186751f0a5b9f12cc3
unknown <jmas14@gcloud.ua.es>
2025-11-13 18:36:40 +0100

    add entrenamiento y callback corregido

--- main.py
+++ main.py
@@ -24,49 +24,7 @@ def plot_curva(Y, titulo, xscale = "linear", yscale = "linear"):
     plt.plot(Y)
     plt.xscale(xscale)
     plt.yscale(yscale)
-    plt.show()
-
-'''
-def plot_Historys(Historys):
-    
-    train_accuracy = []
-    train_loss = []
-    validation_accuracy = []
-    validation_loss = []
-        
-    for h in Historys:
-        train_accuracy.append(h.history['accuracy'])
-        train_loss.append(h.history['loss'])
-        validation_accuracy.append(h.history['val_accuracy'])
-        validation_loss.append(h.history['val_loss'])
-
-    # ====== PLOT ACCURACY ======
-    plt.figure(figsize=(12,5))
-
-    for acc, val_acc in zip(train_accuracy, validation_accuracy):
-        epochs = range(1, len(acc) + 1)
-        plt.plot(epochs, acc, 'b-', alpha=0.4)
-        plt.plot(epochs, val_acc, 'r--', alpha=0.4)
-
-        
-    plt.title("Curvas de accuracy (cada entrenamiento)")
-    plt.xlabel("Época")
-    plt.ylabel("Accuracy")
-    plt.legend(["Entrenamiento", "Validación"])
-
-    # ====== PLOT LOSS ======
-    plt.subplot(1,2,2)
-    for loss, val_loss in zip(train_loss, validation_loss):
-        plt.plot(epochs, loss, 'b-', alpha=0.4)
-        plt.plot(epochs, val_loss, 'r--', alpha=0.4)
-    plt.title("Curvas de loss (cada entrenamiento)")
-    plt.xlabel("Época")
-    plt.ylabel("Loss")
-    plt.legend(["Entrenamiento", "Validación"])
-
-    plt.tight_layout()
-    plt.show()
-'''     
+    plt.show()     
 
 def plot_Historys(Historys):
     plt.figure(figsize=(12, 5))
@@ -125,36 +83,39 @@ def cargar_y_preprocesar_cifar10():
 def main():
         
     X_train, X_test, Y_train, Y_test = cargar_y_preprocesar_cifar10()
-        
-    model = keras.Sequential(
-        [
-            layers.Dense(48, activation="sigmoid", name="layer1"),
-            layers.Dense(10, activation="softmax", name="layer2"),
-        ]
-    )
-    
-    model.compile(
-        optimizer="Adam",
-        loss="categorical_crossentropy",
-        metrics=["accuracy"],
-    )
-    
-    model.summary()
-    
-    my_callbacks = [
-        keras.callbacks.EarlyStopping(patience=2),
-        keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.keras'),
-        keras.callbacks.TensorBoard(log_dir='./logs'),
-    ]
-
     
     Historys = []
-    for i in range(5):
+    for i in range(3):
+        
+        model = keras.Sequential(
+            [
+                keras.Input(X_train[0].shape),
+                layers.Dense(48, activation="sigmoid", name="layer1"),
+                layers.Dense(10, activation="softmax", name="layer2"),
+            ]
+        )
+    
+        model.compile(
+            optimizer="Adam",
+            loss="categorical_crossentropy",
+            metrics=["accuracy"],
+        )
+    
+        model.summary()
+    
+        my_callbacks = [
+            keras.callbacks.EarlyStopping(patience=5, monitor="accuracy", min_delta=0.01),
+            keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.keras'),
+            keras.callbacks.TensorBoard(log_dir='./logs'),
+        ]
+
+        
         print(f"Entrenamiento numero {i+1}")
-        Historys.append(model.fit(X_train, Y_train, batch_size=32, epochs=10, validation_split=0.1,callbacks=my_callbacks))
+        Historys.append(model.fit(X_train, Y_train, batch_size=32, epochs=50, validation_split=0.1,callbacks=my_callbacks))
         test_scores = model.evaluate(X_test, Y_test, verbose=2)
         print("Test loss:", test_scores[0])
         print("Test accuracy:", test_scores[1])
+        
     
     plot_Historys(Historys)
     
